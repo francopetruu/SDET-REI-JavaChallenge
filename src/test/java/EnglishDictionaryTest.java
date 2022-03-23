3d468e372eef96f5e28125dc8d6e26ad8c408a22
Franco Petruccelli <fpetruccelli@symplr.com>
2022-03-22 23:20:32 -0300

    englishdictionary test added

--- src/test/java/EnglishDictionaryTest.java
+++ src/test/java/EnglishDictionaryTest.java
@@ -1,2 +1,15 @@
+import dictionaries.EnglishDictionary;
+import org.junit.Assert;
+import org.junit.Test;
+
 public class EnglishDictionaryTest {
+
+    private EnglishDictionary dictionary;
+
+    @Test
+    public void IsWordOnDictionaryReturnsProperBoolean() {
+        dictionary = new EnglishDictionary();
+        Assert.assertTrue(dictionary.IsWordOnDictionary("KING"));
+        Assert.assertFalse(dictionary.IsWordOnDictionary("afjp"));
+    }
 }
